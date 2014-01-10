b7703c8996728faf04fd142ba45024aefca7d673
niaeashes <nia.eashes@gmail.com>
2014-01-10 18:38:32 +0900

    update estimate form

---
+++ lib/hansode/form_builder.rb
@@ -0,0 +1,20 @@
+class Hansode::FormBuilder < ActionView::Helpers::FormBuilder
+
+  %w(text_field text_area number_field).each do |selector|
+    class_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1
+      def #{selector}(method, options = {})
+        control = super(method, options.except(:label).merge(class: 'form-control'))
+        @template.content_tag(:div, _label_by_options(method, options) + control, class: 'form-group')
+      end
+    RUBY_EVAL
+  end
+
+  def submit(value=nil, options={})
+    super(value, { class: 'btn btn-primary' }.merge(options))
+  end
+
+  private
+    def _label_by_options(method, options)
+      options.has_key?(:label) ? self.label(method, options[:label]) : ''.html_safe
+    end
+end
