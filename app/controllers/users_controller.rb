2dab99b77a3ba80fa7639fb1cbb678a65cd9843e
niaeashes <nia.eashes@gmail.com>
2014-01-15 14:35:26 +0900

    fixed typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.save
-      redirect_to root_rul, notice: "Sign up!"
+      redirect_to root_url, notice: "Sign up!"
     else
       render :new
     end
