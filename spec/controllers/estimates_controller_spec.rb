62d5ab65f5055334346112e781c5c8d973b6f12f
niaeashes <nia.eashes@gmail.com>
2014-02-15 00:53:46 +0900

    Clear spec for estimates#index

--- spec/controllers/estimates_controller_spec.rb
+++ spec/controllers/estimates_controller_spec.rb
@@ -1,8 +1,13 @@
 require 'spec_helper'
 
 describe EstimatesController do
+  # Login with Sorcery Helper
+  before(:all) { @current_user = create :user }
+  before(:each) { login_user @current_user }
+
   describe "GET index" do
     it "has 200 status code" do
+      Estimate.stub(:all)
       get :index
       expect(response).to be_success
     end
